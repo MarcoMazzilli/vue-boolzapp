4a4937b5a11246580fe08472bf494cbd5e88c87e
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-04-18 13:22:00 +0200

    [Fix] Change db `date` format

--- Milestone2/assets/js/dbContacts.js
+++ Milestone2/assets/js/dbContacts.js
@@ -1,3 +1,4 @@
+const dt = luxon.DateTime
 export default [
     {
         ContactName: 'Michele',
@@ -5,12 +6,15 @@ export default [
         visible: true,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: {
+                    dayDate :dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT), 
+                    dayTime :dt.now().toFormat('HH'+':'+'mm')
+                },
                 textMessage: 'Hai portato a spasso il cane?',
                 status: 'sent'
             },
             {
-                date: '10/01/2020 16:15:22',
+                date: dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT) + " " + dt.now().toFormat('HH'+':'+'mm'),
                 textMessage: 'Tutto fatto!',
                 status: 'received'
             },
@@ -22,7 +26,10 @@ export default [
         visible: false,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: {
+                    dayDate :dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT), 
+                    dayTime :dt.now().toFormat('HH'+':'+'mm')
+                },
                 textMessage: 'Hai portato a spasso il cane?',
                 status: 'sent'
             },
@@ -34,7 +41,10 @@ export default [
         visible: false,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: {
+                    dayDate :dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT), 
+                    dayTime :dt.now().toFormat('HH'+':'+'mm')
+                },
                 textMessage: 'Hai portato a spasso il cane?',
                 status: 'sent'
             },
@@ -46,7 +56,10 @@ export default [
         visible: false,
         messages:[
             {
-                date: '10/01/2020 15:30:55',
+                date: {
+                    dayDate :dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT), 
+                    dayTime :dt.now().toFormat('HH'+':'+'mm')
+                },
                 textMessage: 'Hai portato a spasso il cane?',
                 status: 'sent'
             },
@@ -58,7 +71,10 @@ export default [
         visible: false,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: {
+                    dayDate :dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT), 
+                    dayTime :dt.now().toFormat('HH'+':'+'mm')
+                },    
                 textMessage: 'Hai portato a spasso il cane?',
                 status: 'sent'
             },
@@ -70,7 +86,10 @@ export default [
         visible: false,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: {
+                    dayDate :dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT), 
+                    dayTime :dt.now().toFormat('HH'+':'+'mm')
+                },
                 textMessage: 'Hai portato a spasso il cane?',
                 status: 'sent'
             },
@@ -81,7 +100,10 @@ export default [
         profilePic: 'assets/img/avatar_7.jpg',
         visible: false,
         messages:[            {
-            date: '10/01/2020 15:30:55',
+            date: {
+                dayDate :dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT), 
+                dayTime :dt.now().toFormat('HH'+':'+'mm')
+            },         
             textMessage: 'Hai portato a spasso il cane?',
             status: 'sent'
         },],
@@ -92,7 +114,10 @@ export default [
         visible: false,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: {
+                    dayDate :dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT), 
+                    dayTime :dt.now().toFormat('HH'+':'+'mm')
+                },
                 textMessage: 'Hai portato a spasso il cane?',
                 status: 'sent'
             },
